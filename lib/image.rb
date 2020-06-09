8fec07fbf7ffd8a7dff6b7b333a5152c1245f011
Livestronger08 <derek.downie@ttu.edu>
2020-06-09 18:48:46 +0000

    Lesson 20: Luhn 1 failure

--- lib/image.rb
+++ lib/image.rb
@@ -2,12 +2,23 @@ class Image
     attr_accessor :data
 
     def initialize(data)
-        @data = data
+      @data = data
     end
-
+  
     def blur
-        #bad code
-
-        return Image.new(@data)
-    end
+        @data = data
+        @data.each_with_index do |row, row_number|
+            row.each_with_index do |item, col_number|
+                data.each do |found_row_number, found_col_number|
+                    if row_number == found_row_number && col_number == found_col_number
+                    @data[row_number -1][col_number] = 1 unless row_number == 0
+                    @data[row_number +1][col_number] = 1 unless row_number == 0
+                    @data[row_number][col_number -1] = 1 unless col_number == 0
+                    @data[row_number][col_number +1] = 1 unless col_number == 0
+                    return Image.new(@data)
+                    end 
+                end
+            end
+        end
+    end 
 end
